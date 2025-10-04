6773ad387dcd44fc95fd3f74c8dbc55f468c3547
egeyilmaz <2007egeyilmaz@gmail.com>
2025-10-04 20:15:40 +0300

    fixed rust language support

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,20 @@ require("core.lazy")
 require("core.lsp")
 require("config.mappings")
 require("config.autocmd")
+
+require("tiny-inline-diagnostic").setup({
+    -- ...
+    signs = {
+      left = "",
+      right = "",
+      diag = "●",
+      arrow = "    ",
+      up_arrow = "    ",
+      vertical = " │",
+      vertical_end = " └",
+    },
+    blend = {
+      factor = 0.22,
+    },
+    -- ...
+  })
--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -1,28 +1,50 @@
 vim.lsp.enable({
-    "lua_ls",
-    "rust_ls"
+  "lua_ls",
+  "rust_analyzer",
 })
 
-vim.diagnostic.config({
-    virtual_lines = true,
-    -- virtual_text = true,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = true,
-    float = {
-        border = "rounded",
-        source = true,
-    },
-    signs = {
-        text = {
-            [vim.diagnostic.severity.ERROR] = "󰅚 ",
-            [vim.diagnostic.severity.WARN] = "󰀪 ",
-            [vim.diagnostic.severity.INFO] = "󰋽 ",
-            [vim.diagnostic.severity.HINT] = "󰌶 ",
+  vim.lsp.config("rust_analyzer", {
+      cmd = { "rust-analyzer" }, 
+      settings = {
+        ["rust-analyzer"] = {
+          cargo = {
+            allFeatures = true,
+            loadOutDirsFromCheck = true,
+            buildScripts = {
+              enable = true,
+            },
+          },
+          checkOnSave = {
+            command = "clippy",
+          },
+          procMacro = {
+            enable = true,
+          },
         },
-        numhl = {
-            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-            [vim.diagnostic.severity.WARN] = "WarningMsg",
-        },
-    },
-})
+      },
+    })
+    
+
+    vim.diagnostic.config(
+      {
+        underline = false,
+        virtual_text = false,
+        update_in_insert = false,
+        severity_sort = true,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = " ",
+            [vim.diagnostic.severity.WARN] = " ",
+            [vim.diagnostic.severity.HINT] = " ",
+            [vim.diagnostic.severity.INFO] = " ",
+          }
+        }
+      }
+    )
+    
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+      virtual_text = false,
+    })
+
+
+    
